58f4ddeba91ea0e8bc690daf53ad5baf0a235f7f
David Mitchell <balance.it@gmail.com>
2015-04-16 14:40:31 +1200

    adding random reordering of google results

--- chrome_extension/devTools/content.js
+++ chrome_extension/devTools/content.js
@@ -47,7 +47,7 @@ function ads(links, scripts){
 		}
 	}
 	for(var i = 0; i < scripts.length; i++){
-		console.log(isAd(scripts[i].src), scripts[i].src);
+		// console.log(isAd(scripts[i].src), scripts[i].src);
 		if(isAd(scripts[i].src)) {
 			ads.push(scripts[i].src);
 		}
@@ -89,9 +89,61 @@ function isLocalLink(uri){
 
 var ad_pattern = "doubleclick|adclick|pagead|googleadservices|show_ads|pagefair|viglink|idgtn";
 
-
 function isAd(text) {
 	return !!text.match(new RegExp(ad_pattern, "i"));
+}
+
+function orderResults(results){
+	var a_results = []
+	for (var i = 0, result; result = results[i]; i++) {
+		result.setAttribute('order', getResultWeight(result));
+		a_results.push(result);
+	}
+
+	function order(a,b){
+		if (a.getAttribute('order') < b.getAttribute('order')) {
+			return -1;
+		}
+		if (a.getAttribute('order') > b.getAttribute('order')) {
+			return 1;
+		}
+		return 0;
+	}
+
+	return a_results.sort(order);
+}
+
+function getResultWeight(result){
+	//would look up result in local storage
+	var weight = Math.random();
+	return weight;
+}
+
+
+function augmentResults(){
+	if(window.location.hostname == 'www.google.co.nz'){
+		// var viewport = document.getElementById("main");
+		// viewport.setAttribute("style","width:50%;float:left;");
+		if (document.getElementById('imagebox_bigimages')){
+			document.getElementById('imagebox_bigimages').remove();
+		}
+		document.getElementById('rhs_block').innerHTML = '';
+
+		var d = document.createElement("div");
+		d.setAttribute("style","background:#ddd;border:1px solid #ccc;padding:15px;top:-40px;position:relative;");
+
+		// the results of the search (first page)
+		results = document.querySelectorAll(".rc");
+		orderedResults = orderResults(results);
+
+		for (var i = 0, result; result = orderedResults[i]; i++) {
+			var dup = result.cloneNode(true);
+			dup.setAttribute("style","padding: 10px; margin-top:10px;");
+			d.appendChild(dup);
+		}
+		// repurpose the 'more info box' to hold the aumented results
+		document.getElementById('rhs_block').appendChild(d);
+	}
 }
 
 inspectPage();
@@ -99,5 +151,6 @@ inspectPage();
 chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse) {
 		inspectPage();
+		augmentResults();
 	}
 );
