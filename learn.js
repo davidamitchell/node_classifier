d62a84bcdec309d2bd0a9a4a21cffdd83f03d03e
David Mitchell <balance.it@gmail.com>
2015-04-22 21:43:55 +1200

    added debugging etc.

--- learn.js
+++ learn.js
@@ -1,75 +1,59 @@
+var _ = require('lodash');
+var test_sites = ['food.com','annabel-langbein.com','jamieoliver.com','recipes.co.nz']
+var inspected_raw = require('./lib/inspected');
 
-var data = [
-{input: {
-  "ads": 3,
-  "doctype": "other",
-  // "domain": "allrecipes.com",
-  "font family": "Verdana, Arial, sans-serif",
-  "font size": 10.6666669845581,
-  "images": 94,
-  "off site links": 94
-}, output: 'liked'},
-{input: {
-  "ads": 3,
-  "doctype": "html5",
-  // "domain": "chow.com",
-  "font family": "proxima-nova, sans-serif",
-  "font size": 15,
-  "images": 2,
-  "off site links": 4
-}, output: 'disliked'},
-{input: {
-  "ads": 0,
-  "doctype": "html5",
-  // "domain": "annabel-langbein.com",
-  "font family": "nimbus-sans, Arial, Helvetica, sans-serif",
-  "font size": 15,
-  "images": 5,
-  "off site links": 10
-}, output: 'disliked'},
-{input: {
-  "ads": 3,
-  "doctype": "html5",
-  // "domain": "chow.com",
-  "font family": "proxima-nova, sans-serif",
-  "font size": 20,
-  "images": 27,
-  "off site links": 44
-}, output: 'liked'},
-{input: {
-  "ads": 3,
-  "doctype": "html5",
-  // "domain": "chow.com",
-  "font family": "proxima-nova, sans-serif",
-  "font size": 20,
-  "images": 27,
-  "off site links": 44
-}, output: 'liked'}
-];
+var data = [];
+var input, output, c_obj;
+//
+// {
+//     "ads": 3,
+//     "below fold": true,
+//     "domain": "allrecipes.com",
+//     "font family": "Verdana, Arial, sans-serif",
+//     "font size": 10.6666669845581,
+//     "html5": false,
+//     "images": 94,
+//     "liked": false,
+//     "main image": 69696,
+//     "off site links": 94
+// },
 
 
-var test = {
-  "ads": 40,
-  "doctype": "html5",
-  "font family": "Arial, 'Helvetica Neue', Helvetica, sans-serif",
-  "font size": 20,
-  "images": 8,
-  "off site links": 8
-}
+_.each(inspected_raw, function(datum){
+  if (!_.contains(test_sites, datum.domain)) {
 
-test = {
-  "ads": 3,
-  "doctype": "html5",
-  // "domain": "chow.com",
-  "font family": "proxima-nova, sans-serif",
-  "font size": 20,
-  "images": 27,
-  "off site links": 44
-}
+    liked = datum.liked;
+    output = liked ? 'liked' : 'disliked';
+    input = datum;
+
+    c_obj = {
+      input: input,
+      output: output
+    }
+    data.push(c_obj);
+  }
+});
+
+console.log(data);
+
+
+var test_data = [];
+
+_.each(inspected_raw, function(datum){
+  if (_.contains(test_sites, datum.domain)) {
+    test_data.push(datum);
+  }
+});
+
+console.log(test_data);
 
 var Classifier = require('./lib/classifier');
 
-var c = new Classifier();
+var c = new Classifier(false);
 
 c.train(data);
-c.test(test);
+
+_.each(test_data, function(test){
+  c.test(test);
+  console.log('tested ', test.domain, 'liked?: ', test.liked)
+});
